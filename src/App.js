64e1e29430ba2940788a668e0e47e02d758959a6
Mostafa <mosi3883@gmail.com>
2022-05-02 11:54:21 +0430

    complete

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import React, { useState } from 'react';
-import data from './data';
+import React from 'react';
+import questions from './data';
 import SingleQuestion from './Question';
 function App() {
-  return <h2>accordion project setup</h2>;
+  return (
+    <main>
+      <div className='container'>
+        <h3>Questions and answers about login</h3>
+        <section className='info'>
+          {questions.map((question) => (
+            <SingleQuestion key={question.id} {...question} />
+          ))}
+        </section>
+      </div>
+    </main>
+  );
 }
 
 export default App;
--- src/Question.js
+++ src/Question.js
@@ -1,7 +1,21 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+const Question = ({ title, info }) => {
+  const [showInfo, setShowInfo] = useState(false);
+  const toggleShowInfo = () => {
+    setShowInfo((prevShowInfo) => !prevShowInfo);
+  };
+  return (
+    <article className='question'>
+      <header>
+        <h4>{title}</h4>
+        <button className='btn' onClick={toggleShowInfo}>
+          {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
+        </button>
+      </header>
+      {showInfo && <p>{info}</p>}
+    </article>
+  );
 };
 
 export default Question;
